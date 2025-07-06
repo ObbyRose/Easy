d11f42cea96f6bf02889e24fa50ca1566947d92e
Tal Maman <dreapet41s@gmail.com>
2025-07-06 18:38:39 +0300

    Refactor isValid function for improved readability; enhance conditional logic and variable naming for clarity.

--- exercises.js
+++ exercises.js
@@ -128,13 +128,14 @@ function twoSum(nums, target) {
     */
     function isValid(s) {
         const stack = [];
-        const map = { '(' : ')', '{' : '}', '[' : ']' };
-        for(let char of s) {
-            if (map[char]){
+        const map = { "(" : ")" , "[" : "]" , "{" : "}" };
+        for (char of s) {
+            if (map[char]) {
                 stack.push(map[char]);
-            }
-            else if (stack.length === 0 || stack.pop() !== char) {
-                return false;
+            } else {
+                if (stack.length === 0 || stack.pop() !== char) {
+                    return false;
+                }
             }
         }
         return stack.length === 0;
